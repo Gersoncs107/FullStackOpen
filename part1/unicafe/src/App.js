a9f25580f374e812e9ffc66246ab03f769f25842
Gersoncs107 <gerson.silva107@gmail.com>
2025-06-05 19:38:42 -0300

    add solution do Part1 - 1.11 Unicafe exercise

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -38,6 +38,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
 return (
    <div>
     <h1>Give Feedback</h1>
