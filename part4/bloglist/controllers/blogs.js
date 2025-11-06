f8fb7fa8fb26c4ceb2529f28df8dcef83b5f4535
Gersoncs107 <gerson.silva107@gmail.com>
2025-11-06 19:26:21 -0300

    Add solution to Part 4 - 4.2 Blog List exercise

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,106 @@
+const blogsRouter = require('express').Router()
+const { response } = require('express')
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', {
+    username: 1,
+    name: 1,
+    id: 1
+  })
+  response.json(blogs)
+})
+
+blogsRouter.get('/:id', async (request, response, next) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  if (!request.token) {
+    return response.status(401).json({ error: 'token missing' })
+  }
+
+  let decodedToken
+  try {
+    decodedToken = jwt.verify(request.token, process.env.SECRET)
+  } catch (error) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = request.user
+  if (!user) {
+    return response.status(401).json({ error: 'user not found' })
+  }
+
+  if (!body.title || !body.url) {
+    return response.status(400).json({ error: 'title or url missing' })
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author || 'Anonymous',
+    url: body.url,
+    likes: body.likes || 0,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  const populatedBlog = await Blog.findById(savedBlog._id).populate('user', {
+    username: 1,
+    name: 1
+  })
+
+  response.status(201).json(populatedBlog)
+})
+
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const user = request.user
+  if (!user) return response.status(401).json({ error: 'token missing or invalid' })
+
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) return response.status(404).end()
+
+  if (blog.user.toString() !== user._id.toString()) {
+    return response.status(403).json({ error: 'unauthorized' })
+  }
+
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+module.exports = blogsRouter
---
+++ part4/bloglist/utils/list_helper.js
@@ -0,0 +1,63 @@
+const User = require
+
+const dummy = (blogs) => {
+  if (Array.isArray(blogs)) {
+    return 1
+  }
+   return undefined
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+  const favorite = blogs.reduce((prev, current) => (prev.likes > current.likes ? prev : current))
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+  const authorCount = {}
+  blogs.forEach(blog => {
+    authorCount[blog.author] = (authorCount[blog.author] || 0) + 1
+  })
+  const maxAuthor = Object.keys(authorCount).reduce((a, b) => authorCount[a] > authorCount[b] ? a : b)
+  return {
+    author: maxAuthor,
+    blogs: authorCount[maxAuthor]
+  }
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return null
+  const likesCount = {}
+  blogs.forEach(blog => {
+    likesCount[blog.author] = (likesCount[blog.author] || 0) + blog.likes
+  })
+  const maxAuthor = Object.keys(likesCount).reduce((a, b) => likesCount[a] > likesCount[b] ? a : b)
+  return {
+    author: maxAuthor,
+    likes: likesCount[maxAuthor]
+  }
+}
+
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+  usersInDb
+}
